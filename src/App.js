a23f88580e50bc8053a255e4bfafe5ad8f4f3786
zatrimaylova <m.zatrimaylova@gmail.com>
2023-03-26 19:09:09 +0300

    SW-1 added eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-//import logo from './logo.svg';
-//import './App.css';
+/**
+ * @prettier
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>ljkgjhfdsdxgfcghvjbkhklk;'/,.mnbvc'</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
